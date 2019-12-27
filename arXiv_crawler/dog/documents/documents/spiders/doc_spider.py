c2ce979e0505eb01968c5a1e4cb6b0a70e592b19
s1659436 <lawhy0729@gmail.com>
2019-12-27 20:16:19 +0000

    update

--- arXiv_crawler/dog/documents/documents/spiders/doc_spider.py
+++ arXiv_crawler/dog/documents/documents/spiders/doc_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import utils
 
 
 class DogSpider(scrapy.Spider):
@@ -7,26 +6,34 @@ class DogSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://arxiv.org/abs/1001.1232',
+            'https://arxiv.org/abs/1901.00004',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
 
-        # extract the categories
-        cats = response.xpath('//ul/li/b/text()').extract()
-        # remove irrelevant terms from cats
-        typ = 'cs.'
-        cats = [cat.split(' - ') for cat in cats if typ in cat]
+        x_title = '//div[@id="content"]/div[@id="abs"]/h1[@class="title mathjax"]/text()'
+        x_authors = '//div[@id="content"]/div[@id="abs"]/div[@class="authors"]/a/text()'
+        x_abstract = '//div[@id="content"]/div[@id="abs"]/blockquote[@class="abstract mathjax"]/text()'
+        x_primary_subject = '//td[@class="tablecell subjects"]/span[@class="primary-subject"]/text()'
+        x_subjects = '//td[@class="tablecell subjects"]/text()'
 
-        # extract the discriptions
-        dess = response.xpath('//ul/li/div[@class="description"]/text()').extract()
+        title = response.xpath(x_title).extract()
+        authors = response.xpath(x_authors).extract()
+        abstract = response.xpath(x_abstract).extract()
+        primary_subject = response.xpath(x_primary_subject).extract()
+        subjects = response.xpath(x_subjects).extract()
 
-        assert len(dess) == len(cats)
-        with open('cat_n_des.txt', 'w+', encoding='utf-8') as f:
-            for i in range(len(cats)):
-                abbr = cats[i][0]
-                full = cats[i][1]
-                des = dess[i]
-                f.write(abbr + '\t' + full + '\t' + des + '\n')
+        # determine whether or not there are secondary subjects
+        has_secondary = not len(subjects) == 1
+        if has_secondary:
+            subjects = primary_subject + [sub.replace('; ', '') for sub in subjects[1:]]
+
+        yield {
+            "title": title,
+            "authors": authors,
+            "abstract": abstract,
+            "primary_subject": primary_subject,
+            "subjects": subjects
+        }
