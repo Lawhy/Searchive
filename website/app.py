0459b797cd2a3fc1cea589b4dffba0aa58f057e6
zhangdiey <zhangdiey@gmail.com>
2020-01-20 18:21:37 +0000

    update

--- website/app.py
+++ website/app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import datetime as dt
 import json
+import torch
+import numpy as np
+from scipy import spatial
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 app = Flask(__name__)
+
+embeddings_dict = {}
+with open("../../data/glove.6B.50d.txt", 'r') as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vector = np.asarray(values[1:], "float32")
+        embeddings_dict[word] = vector
 
 @app.route('/')
 def home():
@@ -16,31 +29,30 @@ def help():
 def result():
     results = getResult(1)
 
-    startIndex = 0
+    try:
+        startIndex = int(request.args.get('si'))
+    except:
+        startIndex = 0
 
-    numberOfResultsPerPage = 10
+    try:
+        numberOfResultsPerPage = int(request.args.get('rpp'))
+    except:
+        numberOfResultsPerPage = 10
 
-    query = 'deep learning'
-
-    if request.args.get('si'):
-        try:
-            startIndex = int(request.args.get('si'))
-        except:
-            startIndex = 0
-
-    if request.args.get('rpp'):
-        try:
-            numberOfResultsPerPage = int(request.args.get('rpp'))
-        except:
-            numberOfResultsPerPage = 10
-
-    if request.args.get('q'):
-        try:
-            query = request.args.get('q')
-        except:
-            query = 'deep learning'
+    try:
+        query = request.args.get('q')
+        if not query.strip():
+            return redirect('/')
+    except:
+        return redirect('/')
 
     numberOfResults = len(results)
+
+    # if len(query.split()) <= 5:
+    #     closest_words = {}
+    #     for idx, word in enumerate(query.split()):
+    #         closest_words[idx] = find_closest_embeddings(embeddings_dict[word])[1]
+    #     print(closest_words)
 
     return render_template('result.html', datetime=dt, title='result', results=results[startIndex:startIndex+numberOfResultsPerPage]
     , startIndex=startIndex, numberOfResultsPerPage=numberOfResultsPerPage, numberOfResults=numberOfResults, query=query)
@@ -53,6 +65,9 @@ def getResult(q):
     with open('../../data/test.json') as f:
         data = json.load(f)
     return data
+
+def find_closest_embeddings(embedding):
+    return sorted(embeddings_dict.keys(), key=lambda word: spatial.distance.euclidean(embeddings_dict[word], embedding))
 
 if __name__ == '__main__':
     app.run(debug=True)
