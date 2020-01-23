412af2caacdaa601edaf5d2f52205219208c653c
s1891132 <s1891132@ed.ac.uk>
2020-01-23 20:10:06 +0000

    indexing draft

--- ranking/rank_base.py
+++ ranking/rank_base.py
@@ -2,6 +2,7 @@ import collections
 import json
 import re
 import time
+import numpy as np
 
 from nltk.stem import *
 from nltk.corpus import stopwords
@@ -17,11 +18,8 @@ def read(filepath):
 def parse(json_text):
     word_dict={}
     text = json.loads(json_text)
-    #print(type(text))
-    #print(text.keys())
     for key in text.keys():
-        doc_id = key
-        #title =  text[key]["title"]
+        doc_id = key    #string
         abstract = text[key]["abs"]
         """ preprocessing """
         tokenised_text = re.findall("\w+",abstract)  # split on all the non-letter words, keep the words consisting of numbers or/and letters
@@ -30,34 +28,59 @@ def parse(json_text):
         stemmer = PorterStemmer()
         stemmed_text = [stemmer.stem(word) for word in stopped_text]  # stemming
 
+
+        # {word : {"df": xx, "docdict":{doc_id 1:{"tf": yy, "pos": [postlist]} , doc_id 2}}}
+
+
         """ create the positional inverted index """
         for pos, word in enumerate(stemmed_text):
             if word not in word_dict:
-                word_dict[word] = {doc_id: [pos + 1]}
+                word_dict[word] = {}
+                word_dict[word]["df"] = 1
+                word_dict[word]["docdict"]= {doc_id:{"pos" : [pos+1]}}
+                word_dict[word]["docdict"][doc_id]["tf"] = 1
+                #word_dict[word] = {"docdict":{doc_id: {"pos":[pos + 1]}}}
+                #word_dict[word]["docdict"][doc_id].update({"tf":1})
+                #word_dict[word]["df"]=1
             else:
-                if doc_id not in word_dict[word]:
-                    word_dict[word].update({doc_id: [pos + 1]})
+                word_dict[word]["df"] = word_dict[word]["df"] + 1
+                if doc_id not in word_dict[word]["docdict"]:
+                    word_dict[word]["docdict"][doc_id]={"pos":[pos + 1]}
+                    word_dict[word]["docdict"][doc_id]["tf"] = 1
+
+
+                    #word_dict[word]["docdict"].update({doc_id: {"pos":[pos + 1]}})
+                    #word_dict[word]["docdict"][doc_id].update({"tf":1})
                 else:
-                    word_dict[word][doc_id].append(pos + 1)
+                    word_dict[word]["docdict"][doc_id]["pos"].append(pos + 1)
+                    word_dict[word]["docdict"][doc_id]["tf"] = word_dict[word]["docdict"][doc_id]["tf"]  + 1
     return word_dict
 
 def write(filepath,word_dict):
-    with open(filepath,"w") as f_wrindex:  # the third input argument in the command line is the text file path you want to write the index in
-        for word, doc in word_dict.items():
-            f_wrindex.write(word + ":")
-            f_wrindex.write("\n")
-            for doc_id, pos_list in doc.items():
-                f_wrindex.write("\t")
-                f_wrindex.write(doc_id + ": ")
-                f_wrindex.write(",".join([str(pos) for pos in pos_list]))
-                f_wrindex.write("\n")
-            f_wrindex.write("\n")
+    #np.save(filepath,word_dict)
+    with open(filepath,"w") as f_wrindex:
+         json.dump(word_dict,f_wrindex)
     f_wrindex.close()
+    #     # for word, data in word_dict.items():
+    #     #     f_wrindex.write(word + ":")
+    #     #     f_wrindex.write(str(data["df"]))
+    #     #     f_wrindex.write("\n")
+    #     #
+    #     #     for doc_id, doc_inf in data["docdict"].items():
+    #     #         f_wrindex.write("\t")
+    #     #         f_wrindex.write(doc_id + ": ")
+    #     #         f_wrindex.write(str(doc_inf["tf"]))
+    #     #         f_wrindex.write(" ")
+    #     #         pos_list = doc_inf["pos"]
+    #     #         f_wrindex.write(",".join([str(pos) for pos in pos_list]))
+    #     #         f_wrindex.write("\n")
+    #     #     f_wrindex.write("\n")
+
 
 start_time = time.time()
 text = read("/afs/inf.ed.ac.uk/user/s18/s1891132/Downloads/1901.json")
 stopwords= set(stopwords.words('english'))
 index_dict= parse(text)
-sorted_word_dict = collections.OrderedDict(sorted(index_dict.items()))
-write("/afs/inf.ed.ac.uk/user/s18/s1891132/Downloads/index.txt",sorted_word_dict)
+#sorted_word_dict = collections.OrderedDict(sorted(index_dict.items()))
+write("/afs/inf.ed.ac.uk/user/s18/s1891132/Downloads/index.json",index_dict)
 print("--- %s seconds ---" % (time.time() - start_time))
