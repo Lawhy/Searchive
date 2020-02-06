2b11eda0e01ffff14e271ae950b9bf73b37ba789
Ling Lu <s1926829@matura.inf.ed.ac.uk>
2020-02-06 20:18:50 +0000

    bm25 & tfidf update

--- ranking/bm25.py
+++ ranking/bm25.py
@@ -1,10 +1,12 @@
 import json
 import math
-import time
-import re
+import search
 
 from nltk.stem import *
 from nltk.corpus import stopwords
+
+docid_set = search.query_docid
+query = search.preprotext
 
 def read(file):
     with open(filepath,"r") as f:
@@ -12,49 +14,36 @@ def read(file):
         f.close()
     return text
 
-def get_query(filepath_query):
-    query = read(filepath_query)
-    """ preprocessing """
-    tokenised_text = re.findall("\w+",query)  # split on all the non-letter words, keep the words consisting of numbers or/and letters
-    lowered_text = [w.lower() for w in tokenised_text]  # case folding
-    stopped_text = [word for word in lowered_text if word not in stopwords]  # remove stop words
-    stemmer = PorterStemmer()
-    stemmed_text = [stemmer.stem(word) for word in stopped_text]  # stemming
-    return stemmed_text
+def bm25_rank(filepath2,text):
+    dict_weight = []
+    detail = read(filepath2)
+    score = 0
+    for docid in docid_set:
+        for term in query:
+            df = text[term]["df"]
+            tf = text[term]["docdict"][docid]["tf"]
+            score = score + score_cal(docid,term,detail,df,tf)
+        dict_weight.append((docid,score))
+        score = 0
+    return dict_weight
 
-def bm25_rank(filepath_query, json_text):
+def score_cal(docid,term,detail,df,tf):
     k1 = 2
     b = 0.75
-    bm25_dict = {}
-    detail = read(filepath2)
-    text = json.loads(json_text)
-    avg_docs = detail["avg_doc"]
-    query_no = 1000
-    query = get_query(filepath_query)
-    total_score = 0
-    for word in query:
-        if word in text.keys():
-            df = text[word]["df"]
-            idf = math.log((detail["sum_doc"]-float(df)+0.5)/(float(df)+0.5),10)
-            score = 0
-            for keys in text[word]["docdict"].keys():
-                len_doc = detail[keys]
-                tf = text[word]["docdict"][keys]["tf"]
-                score += float(tf*(k1+1))/float(tf+k1*(1-b+b*float(len_doc)/float(avg_docs)))
-        total_score = idf*score
-    print("query: %f total_score: %f" % query_no, total_score)
-    bm25_dict[query_no] = total_score
+    idf = math.log((detail["sum_doc"]-float(df)+0.5)/(float(df)+0.5),10)
+    len_doc = detail[docid]
+    tf = text[term]["docdict"][docid]["tf"]
+    score = idf * float(tf*(k1+1))/float(tf+k1*(1-b+b*float(len_doc)/float(detail["avg_doc"])))
+    return score
 
 def write(filepath,rank_dict):
     with open(filepath,"w") as f_wrindex:
          json.dump(rank_dict,f_wrindex)
     f_wrindex.close()
     
-start_time = time.time()
 text = read(filepath)
-bm25_dict = bm25_rank(text)
-write(filepath3,rank_dict)
-print("--- %s seconds ---" % (time.time() - start_time))
+bm25_dict = bm25_rank(filepath2,text)
+write(filepath3,bm25_dict)
 
 # bm25_dict = {"sum_doc":???,"doc_id 1":length,"doc_id 2": length}
 
--- ranking/tfidf.py
+++ ranking/tfidf.py
@@ -1,10 +1,12 @@
 import json
 import math
-import time
-import re
+import search
 
 from nltk.stem import *
 from nltk.corpus import stopwords
+
+docid_set = search.query_docid
+query = search.preprotext
 
 def read(file):
     with open(filepath,"r") as f:
@@ -12,15 +14,17 @@ def read(file):
         f.close()
     return text
 
-def get_query(filepath_query):
-    query = read(filepath_query)
-    """ preprocessing """
-    tokenised_text = re.findall("\w+",query)  # split on all the non-letter words, keep the words consisting of numbers or/and letters
-    lowered_text = [w.lower() for w in tokenised_text]  # case folding
-    stopped_text = [word for word in lowered_text if word not in stopwords]  # remove stop words
-    stemmer = PorterStemmer()
-    stemmed_text = [stemmer.stem(word) for word in stopped_text]  # stemming
-    return stemmed_text
+def rank(query_word,text):
+    dict_weight = []
+    score = 0
+    for docid in docid_set:
+        for term in query_word:
+            df = text[term]["df"]
+            tf = text[term]["docdict"][docid]["tf"]
+            score = score + score_cal(df,tf)
+        dict_weight.append((docid,score))
+        score = 0
+    return dict_weight
     
 def score_cal(df,tf):
     if df != 0 and tf != 0:
@@ -28,34 +32,14 @@ def score_cal(df,tf):
         return w
     else:
         return 0
-    
-def rank(filepath_query,json_text):
-    query_stem = get_query(filepath_query)
-    rank_dict = {}
-    text = json.loads(json_text)
-    total_score = 0
-    query_no = 1000
-    for word in query_stem:
-        if word in text.keys():
-            df = text[word]["df"]
-            score = 0
-            for keys in text[word]["docdict"].keys():
-                tf = text[word]["docdict"][keys]["tf"]
-                score += score_cal(df,tf)
-        total_score += score
-    print("query: %f total_score: %f" % query_no, total_score)
-    rank_dict[query_no] = total_score
 
 def write(filepath,rank_dict):
     with open(filepath,"w") as f_wrindex:
          json.dump(rank_dict,f_wrindex)
     f_wrindex.close()
 
-query = get_query(filepath_query)
-start_time = time.time()
 text = read(filepath)
-rank_dict = rank(text)
-write(filepath,rank_dict)
-print("--- %s seconds ---" % (time.time() - start_time))
-
+dict_w = rank(query,text)
+dict_w_sort = sorted(dict_w,key = lambda x:x[1], reverse = True)
+write(filepath,dict_w_sort)
 # {word : {"df": xx, "docdict":{doc_id 1:{"tf": yy, "pos": [postlist]} , doc_id 2}}}
