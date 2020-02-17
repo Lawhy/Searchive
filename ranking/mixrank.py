a6727373dfd8804bea147d024746ebda968eb260
Ling Lu <s1926829@reerslev.inf.ed.ac.uk>
2020-02-17 21:22:44 +0000

    tfidf & bm25 using index

--- ranking/mixrank.py
+++ ranking/mixrank.py
@@ -2,6 +2,7 @@ import json
 import math
 import search
 import collections
+import readindex
 
 docid_set = search.query_docid
 query = search.preprotext
@@ -13,18 +14,6 @@ def read(file):
         f.close()
     return text
 
-def tfidf_rank(query_word,text):
-    dict_weight = {}
-    score = 0
-    for docid in docid_set:
-        for term in query_word:
-            df = text[term]["df"]
-            tf = text[term]["docdict"][docid]["tf"]
-            score = score + tfidf_score_cal(df,tf)      
-        dict_weight[docid] = score
-        score = 0
-    return dict_weight
-    
 def tfidf_score_cal(df,tf):
     if df != 0 and tf != 0:
         w = (1 + math.log(float(tf),10))*math.log(5000/float(df),10)
@@ -32,26 +21,14 @@ def tfidf_score_cal(df,tf):
     else:
         return 0
 
-def bm25_rank(bm25,text):
-    dict_weight = {}
-    detail = bm25
-    score = 0
-    for docid in docid_set:
-        for term in query:
-            df = text[term]["df"]
-            tf = text[term]["docdict"][docid]["tf"]
-            score = score + bm25_score_cal(docid,term,detail,df,tf)
-        dict_weight[docid] = score
-        score = 0
-    return dict_weight
-
-def bm25_score_cal(docid,term,detail,df,tf):
+def bm25_score_cal(detail,df,tf):
     k1 = 2
     b = 0.75
-    idf = math.log((detail["sum_doc"]-float(df)+0.5)/(float(df)+0.5),10)
-    len_doc = detail[docid]
-    tf = text[term]["docdict"][docid]["tf"]
-    score = idf * float(tf*(k1+1))/float(tf+k1*(1-b+b*float(len_doc)/float(detail["avg_doc"])))
+    len_doc = detail["total_document_length"]
+    no_doc = detail["total_doc_number"]
+    avg_doc = float(len_doc/no_doc)
+    idf = math.log((no_doc-float(df)+0.5)/(float(df)+0.5),10)
+    score = idf * float(tf*(k1+1))/float(tf+k1*(1-b+b*float(len_doc)/avg_doc))
     return score
 
 def weight_score(bm25,tfidf):
@@ -63,6 +40,31 @@ def weight_score(bm25,tfidf):
         dict_final.append((docid,score))
     return dict_final
 
+def rank(query):
+    dict_tfidf = {}
+    dict_bm25 = {}
+    dict_final = []
+    tfidf_score = 0
+    bm25_score = 0
+    for docid in docid_set:
+        for term in query:
+            dict_term = readindex.read_index(term,"abstract")
+            dict_detail = readindex.read_index(term,"param")
+            
+            docid.replace('.','-')
+            df = dict_term["df"]
+            tf = dict_term[docid]['tf']
+            
+            tfidf_score = tfidf_score + tfidf_score_cal(df,tf)
+            bm25_score = bm25_score + bm25_score_cal(dict_detail,df,tf)
+        docid.replace('-','.')
+        dict_tfidf[docid] = tfidf_score
+        dict_bm25[docid] = bm25_score
+        tfidf_score = 0
+        bm25_score = 0
+    dict_final = weight_score(dict_bm25,dict_tfidf)
+    return dict_final
+
 def search_for_detail(dict_w_sort):
     dict_result = collections.OrderedDict()
     for each_list in dict_w_sort:
@@ -72,22 +74,12 @@ def search_for_detail(dict_w_sort):
         dict_result[each_list[0]] = dict_1901[each_list[0]]
         dict_result[each_list[0]]['score'] = each_list[1]
     return dict_result
-  
-def write(filepath,rank_dict):
-    with open(filepath,"w") as f_wrindex:
-         json.dump(rank_dict,f_wrindex)
-    f_wrindex.close()
 
-text = read('/afs/inf.ed.ac.uk/user/s19/s1926829/TTDS/test/index1901.json')
-bm25 = read('/afs/inf.ed.ac.uk/user/s19/s1926829/TTDS/test/index25.json')
+def getResult(query):
+    dict_final = rank(query)
+    dict_final_w = sorted(dict_final, key = lambda x:x[1], reverse = True)
+    dict_result = search_for_detail(dict_final_w)
+    return dict_result
 
-dict_w = tfidf_rank(query,text)
-
-bm25_dict = bm25_rank(bm25,text)
-
-dict_final = weight_score(bm25_dict,dict_w)
-dict_final_w = sorted(dict_final, key = lambda x:x[1], reverse = True)
-
-dict_result = search_for_detail(dict_final_w)
-write('/afs/inf.ed.ac.uk/user/s19/s1926829/TTDS/test/rank_tfidf_1901_detail.json',dict_result)
-# {word : {"df": xx, "docdict":{doc_id 1:{"tf": yy, "pos": [postlist]} , doc_id 2}}}
+#{term:{docid1:{'pos':[],'tf':??},docid2:{},'df':??},term:{}}
+#{term:{total_doc_number:??,total_doc_length:??},term:{}}
