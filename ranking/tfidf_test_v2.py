8a3e3e50cf051b27e42a8b51df4e4cdbe490ace6
Ling Lu <s1926829@bobet.inf.ed.ac.uk>
2020-02-19 20:24:17 +0000

    speed up

--- ranking/tfidf_test_v2.py
+++ ranking/tfidf_test_v2.py
@@ -2,6 +2,7 @@ import math
 import sys
 import json
 import time
+import collections
 
 sys.path.append('..')  # append the main directory path
 from search.search import mode_select, preprocess_squery
@@ -15,61 +16,66 @@ def read(filepath):
     return text
 
 def tfidf_score_cal(df,tf):
-    if df != 0 and tf != 0:
-        w = (1 + math.log(float(tf),10))*math.log(5000/float(df),10)
-        return w
-    else:
-        return 0
+    w = 1 + math.log10(float(tf)) * math.log10(5000/float(df))
+    return w
 
 def rank(raw_query,query,mode):
     docid_set = mode_select(raw_query,mode)
+    dict_term = {}
     if docid_set == "None":
         return "None"
     else:
         dict_tfidf = []
-        tfidf_score = 0
-        start_time5 = time.time()
+        for term in query:
+            if read_index(term,mode) != None:
+                dict_term[term] = read_index(term,mode)
+        
         for docid in docid_set:
-            for term in query:
-                if read_index(term,mode) != None:
-                    dict_term = read_index(term, mode)
-                    df = dict_term["df"]
-                    if docid in dict_term.keys():
-                        tf = dict_term[docid]['tf']
-                    else:
-                        tf = 0
-                else:
-                    df = 0
-                    tf = 0
-                tfidf_score = tfidf_score + tfidf_score_cal(df, tf)
-            dict_tfidf.append((docid,tfidf_score))
             tfidf_score = 0
+            for term in dict_term.keys():
+                # whether term exists in index
+                df = dict_term[term]["df"]
+                if docid in dict_term[term].keys():
+                    tf = dict_term[term][docid]['tf']
+                    tfidf_score += tfidf_score_cal(df,tf)
+                else:
+                    tfidf_score = tfidf_score
+            dict_tfidf.append((docid,tfidf_score))
         dict_tfidf_sort = sorted(dict_tfidf, key = lambda x:x[1], reverse = True)
-        print("--- for loop: %s seconds ---" % (time.time() - start_time5))
         return dict_tfidf_sort
 
+def cate(dict_final):
+    dict_classi = collections.defaultdict(list)
+    for each_list in dict_final:
+        doc_id_f = each_list[0].replace('-','.')
+        doc_id = int(float(doc_id_f))
+        dict_classi[doc_id].append(doc_id_f)
+    return dict_classi #{json name:[doci_id1,doc_id2,...]}
+
 def search_for_detail(raw_query,mode="abstract"):
-    start_time1 = time.time()
     query = preprocess_squery(raw_query,mode)
-    print("--- preprocess the query: %s seconds ---" % (time.time() - start_time1))
-    start_time2 = time.time()
+    # ordered list of tuples [(doc_id, score),()]
     dict_final = rank(raw_query,query,mode)
-    print("--- ranking process: %s seconds ---" % (time.time() - start_time2))
-    
+    dict_result = {}
+    result_final_all = []
     if dict_final == 'None':
         return []
     else:
-        result_list = []
-        start_time3 = time.time()
-        for each_list in dict_final:
-            dict_result_temp = {}
-            doc_id_p = each_list[0].replace('-','.')
-            doc_id = int(float(doc_id_p))
-            filepath = '../../data/' + str(doc_id) + '.json'
+        dict_classi = cate(dict_final)
+        # get all details of all doc_id but not in order
+        for key in dict_classi.keys():
+            filepath =  '../../data/' + str(key) + '.json'
             dict_1907 = read(filepath)
-            dict_result_temp = dict_1907[doc_id_p]
-            dict_result_temp["id"] = doc_id_p
-            dict_result_temp['score'] = each_list[1]
-            result_list.append(dict_result_temp)
-        print("--- search for detail: %s seconds ---" % (time.time() - start_time3))
-        return result_list
+            for doc_id_temp in dict_classi[key]:
+                dict_result[doc_id_temp] = dict_1907[doc_id_temp]
+        # order the results
+        for item in dict_final:
+            doc_id_norm = item[0].replace('-','.')
+            dict_result_temp = dict_result[doc_id_norm]
+            dict_result_temp["id"] = doc_id_norm
+            dict_result_temp['score'] = item[1]
+            result_final_all.append(dict_result_temp)
+        # limit the number of documents shown to users
+        if len(result_final_all) > 1000:
+            result_final_all = result_final_all[:1000]
+        return result_final_all
