daaa16172cd73f463387bf49165471ebdc5100fd
李杰宁 <AlisonLee@pc-77-176.customer.ask4.lan>
2020-02-19 20:29:04 +0000

    fix minor problems

--- indexing/readindex_mongo.py
+++ indexing/readindex_mongo.py
@@ -11,9 +11,5 @@ mode = 'abstract' / 'title' / 'author'/ 'param'
 
 def read_index(word, mode):
     coll_ref = db[mode]
-
     doc_dict = coll_ref.find_one({"_id":word})
-    print(doc_dict)
     return doc_dict
-
-read_index("gama","title")
