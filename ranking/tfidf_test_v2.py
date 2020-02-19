d09c1f04d1c25cf7629ea354e281a498b908db35
Ling Lu <s1926829@archlute.inf.ed.ac.uk>
2020-02-19 15:53:32 +0000

    update

--- ranking/tfidf_test_v2.py
+++ ranking/tfidf_test_v2.py
@@ -28,22 +28,24 @@ def rank(raw_query,query,mode):
     else:
         dict_tfidf = []
         tfidf_score = 0
+        start_time5 = time.time()
         for docid in docid_set:
             for term in query:
                 if read_index(term,mode) != None:
                     dict_term = read_index(term, mode)
                     df = dict_term["df"]
+                    if docid in dict_term.keys():
+                        tf = dict_term[docid]['tf']
+                    else:
+                        tf = 0
                 else:
                     df = 0
-
-                if docid in dict_term.keys():
-                    tf = dict_term[docid]['tf']
-                else:
                     tf = 0
                 tfidf_score = tfidf_score + tfidf_score_cal(df, tf)
             dict_tfidf.append((docid,tfidf_score))
             tfidf_score = 0
         dict_tfidf_sort = sorted(dict_tfidf, key = lambda x:x[1], reverse = True)
+        print("--- for loop: %s seconds ---" % (time.time() - start_time5))
         return dict_tfidf_sort
 
 def search_for_detail(raw_query,mode="abstract"):
